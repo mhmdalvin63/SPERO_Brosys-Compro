48c2fd7f8c395d5a9e2e98e6ec629c366a50cb88
muhammadalvin92 <alvinbomber87@gmail.com>
2023-11-20 15:18:15 +0700

    update

--- resources/views/Navbar.blade.php
+++ resources/views/Navbar.blade.php
@@ -7,22 +7,16 @@
       <div class="navbar-collapse" id="navbarText">
         <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-md-5 gap-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#sect-1"><p>1</p></a>
+            <a class="nav-link active" aria-current="page" href="#sect-1"><p>Home</p></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#sect-2"><p>2</p></a>
+            <a class="nav-link" href="#sect-3"><p>Profile</p></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#sect-3"><p>3</p></a>
+            <a class="nav-link" href="#sect-5"><p>Event</p></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#sect-4"><p>4</p></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#sect-5"><p>5</p></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#sect-6"><p>6</p></a>
+            <a class="nav-link" href="#sect-6"><p>Lorem Ipsum</p></a>
           </li>
         </ul>
       </div>
